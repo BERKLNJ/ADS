3fcfba867546c7b9bada95715e7d300b4f2884b8
BERKLNJ <subanovbayel@gmail.com>
2026-03-27 12:34:17 +0500

    part 1

--- src/Task6.java
+++ src/Task6.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class Task6 {
-    public static long power(int a, int n) {
-        if (n == 0) return 1;
-        return a * power(a, n - 1);
+
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -15,5 +13,7 @@ public class Task6 {
         scanner.close();
     }
 
-
+    public static long power(int a, int n) {
+        if (n == 0) return 1;
+        return a * power(a, n - 1);
 }
